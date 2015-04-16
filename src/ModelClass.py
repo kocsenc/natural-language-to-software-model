b5607702c2fffe8b109b4386474b5014c8131b9d
Kocsen <kocsenc@gmail.com>
2015-04-16 13:00:46 -0400

    - model class push

---
+++ src/ModelClass.py
@@ -0,0 +1,10 @@
+__author__ = 'kocsen'
+
+
+class ModelClass():
+    """
+    Class that represents an actual class in software modelling
+    """
+    def __init__(self):
+        self.attributes = []
+        self.behaviours = []
