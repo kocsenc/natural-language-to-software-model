a10a390fdbaebd7928d4daa56f1fae35aa5ed03b
David Kisluk <david.kisluk@gmail.com>
2015-05-01 18:58:46 -0400

    Parse args with getopt

--- src/driver.py
+++ src/driver.py
@@ -1,8 +1,24 @@
 #!usr/bin/env python3
 
+import sys, getopt
 
-def main():
+def main(argv):
     # parse args
+    input_file = ''
+    output_file = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print_help()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print_help()
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            input_file = arg
+        elif opt in ("-o", "--ofile"):
+            output_file = arg
 
     # In comes the text (filename)
     # Use nltk to tokenize text.
@@ -18,7 +34,9 @@ def main():
 
     pass
 
+def print_help():
+    print('driver.py -i <inputfile> -o <outputfile>')
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
 
